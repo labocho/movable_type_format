3d4f93653a31c016003e7570c8fa6832476a0c0a
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-03-09 20:38:19 +0100

    Added a few date tests

--- spec/movable_type_format/entry_spec.rb
+++ spec/movable_type_format/entry_spec.rb
@@ -1,18 +1,21 @@
 # encoding: UTF-8
 require 'spec_helper'
+require 'json'
 
 module MovableTypeFormat
   describe Entry do
-    it "build" do
+    it "to_mt" do
       e = Entry.new
       e.title = "Title"
       e.author = "Author"
       e.body = "Body\nBody"
+      e.date = Time.new(2003, 6, 23, 23, 34, 22)
       e.sections << Section::Comment.new({author: "Comment Author"}, "Comment")
 
       expected = <<-MT
       TITLE: Title
       AUTHOR: Author
+      DATE: 06/23/2003 23:34:22
       -----
       BODY:
       Body
@@ -27,6 +30,18 @@ module MovableTypeFormat
       expected.gsub!(/^\s+/, "")
 
       e.to_mt.should == expected
+    end
+    it "to_json" do
+      e = Entry.new
+      e.title = "Title"
+      e.author = "Author"
+      e.body = "Body\nBody"
+      e.date = Time.new(2003, 6, 23, 23, 34, 22)
+      e.sections << Section::Comment.new({author: "Comment Author"}, "Comment")
+
+      expected = "{\"body\":\"Body\\nBody\",\"author\":\"Author\",\"title\":\"Title\",\"date\":\"2003-06-23 23:34:22 +0200\",\"comments\":[{\"author\":\"Comment Author\",\"body\":\"Comment\"}]}"
+
+      e.serialize.to_json.should == expected
     end
   end
 end
