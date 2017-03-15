9ee0dc5d455d6569fc13f6ec42a227142b2f4568
labocho <labocho@penguinlab.jp>
2017-03-15 11:43:02 +0900

    fix test depends time zone

--- spec/movable_type_format/entry_spec.rb
+++ spec/movable_type_format/entry_spec.rb
@@ -36,10 +36,10 @@ module MovableTypeFormat
       e.title = "Title"
       e.author = "Author"
       e.body = "Body\nBody"
-      e.date = Time.new(2003, 6, 23, 23, 34, 22)
+      e.date = Time.utc(2003, 6, 23, 23, 34, 22)
       e.sections << Section::Comment.new({author: "Comment Author"}, "Comment")
 
-      expected = "{\"body\":\"Body\\nBody\",\"author\":\"Author\",\"title\":\"Title\",\"date\":\"2003-06-23 23:34:22 +0200\",\"comments\":[{\"author\":\"Comment Author\",\"body\":\"Comment\"}]}"
+      expected = "{\"body\":\"Body\\nBody\",\"author\":\"Author\",\"title\":\"Title\",\"date\":\"2003-06-23 23:34:22 UTC\",\"comments\":[{\"author\":\"Comment Author\",\"body\":\"Comment\"}]}"
 
       e.serialize.to_json.should == expected
     end
